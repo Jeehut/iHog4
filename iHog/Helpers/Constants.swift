73b478fce5072d5ee409dc8484070f3457504541
Maegan <maegan.wilson@highend.com>
2020-10-21 07:52:29 -0500

    Working on issue #15

--- iHog/Helpers/Constants.swift
+++ iHog/Helpers/Constants.swift
@@ -35,4 +35,36 @@ let XL_OBJ_BUTTON_SIZE: CGFloat     = 200.0
 // MARK: SETTING ENUMS
 enum Settings {
     static let chosenShowID = "chosenShowID"
+    
+    static let hognetIPAddress = "172.31.0.1"
+    static let inputPort = 7001
+    static let outputPort = 7002
+    static let isOSCOn = false
+    
+    static let encoderWheelPrecision = 2.0
+    static let isNanoModeOn = false
+    
+    // MARK: Button Group
+    static let buttonColorGroup = "red"
+    static let buttonSizeGroup = "small"
+    static let buttonsAcrossGroup = 3
+    static let isButtonFilledGroup = true
+    
+    // MARK: Button Palette
+    static let buttonColorPalette = "blue"
+    static let buttonSizePalette = "small"
+    static let buttonsAcrossPalette = 3
+    static let isButtonFilledPalette = true
+    
+    // MARK: Button List
+    static let buttonColorList = "green"
+    static let buttonSizeList = "small"
+    static let buttonsAcrossList = 3
+    static let isButtonFilledList = true
+    
+    // MARK: Button Scene
+    static let buttonColorScene = "gray"
+    static let buttonSizeScene = "small"
+    static let buttonsAcrossScene = 3
+    static let isButtonFilledScene = true
 }
