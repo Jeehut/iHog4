eb1bf2e201476c1a6ec322379da32ae4acb7b93a
Maegan <maegan.wilson@highend.com>
2020-12-03 07:17:41 -0600

    Can now save. This closes issue #21

--- iHog/Views/ShowObjects/EditObjectView.swift
+++ iHog/Views/ShowObjects/EditObjectView.swift
@@ -6,8 +6,10 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct EditObjectView: View {
+    @Environment(\.managedObjectContext) private var viewContext
     @Environment(\.presentationMode) private var presentationMode
     @Binding var allObjects: [ShowObject]
     
@@ -21,13 +23,19 @@ struct EditObjectView: View {
     var body: some View {
         VStack{
             HStack{
+                Button(action: {self.presentationMode.wrappedValue.dismiss()}){
+                    Text("Cancel")
+                }
+                .foregroundColor(.red)
+                .padding(.horizontal)
                 Spacer()
                 Button(action: {
                     self.presentationMode.wrappedValue.dismiss()
+                    saveValues()
                 }) {
-                    Text("Close")
+                    Text("Save")
                 }
-                .foregroundColor(.red)
+                .foregroundColor(.green)
                 .padding(.horizontal)
             }.padding(.vertical)
             Form{
@@ -50,6 +58,7 @@ struct EditObjectView: View {
         }.onAppear{
             getInitialValues()
         }
+//        .onDisappear(perform: saveValues)
     }
     
     func getInitialValues() {
@@ -59,7 +68,25 @@ struct EditObjectView: View {
         objColor = obj.getColor()
     }
     func saveValues(){
-        print("SAVE VALUES")
+        let updatedOBJ = ShowObject(id: obj.id, objType: obj.objType, number: obj.number, name: name, objColor: OBJ_COLORS[objColor].description, isOutlined: isOutlined)
+        let savedOBJID: NSUUID = obj.id as NSUUID
+        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "ShowObjectEntity")
+        fetchRequest.predicate = NSPredicate(format: "id == %@", savedOBJID as CVarArg)
+        fetchRequest.fetchLimit = 1
+        do {
+            let test = try viewContext.fetch(fetchRequest)
+            let objectToUpdate = test[0] as! NSManagedObject
+            objectToUpdate.setValue(name, forKey: "name")
+            objectToUpdate.setValue(OBJ_COLORS[objColor].description, forKey: "objColor")
+            objectToUpdate.setValue(isOutlined, forKey: "isOutlined")
+            try viewContext.save()
+            allObjects.removeAll{$0 == obj}
+            allObjects.append(updatedOBJ)
+            allObjects.sort(by: {$0.number  < $1.number})
+        } catch {
+            print(error)
+        }
+        
     }
 }
 
