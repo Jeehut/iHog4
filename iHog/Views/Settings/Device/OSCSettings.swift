3bd5e6f74d8df81b282b529b35a0d0fdd8b0910d
Maegan Wilson <mwilson@maegans-mini.lan>
2021-03-24 07:25:29 -0500

    [IHOG-76] Close issue. Broke it down into files

---
+++ iHog/Views/Settings/Device/OSCSettings.swift
@@ -0,0 +1,20 @@
+//
+//  OSCSettings.swift
+//  iHog
+//
+//  Created by Maegan Wilson on 3/24/21.
+//
+
+import SwiftUI
+
+struct OSCSettings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OSCSettings_Previews: PreviewProvider {
+    static var previews: some View {
+        OSCSettings()
+    }
+}
---
+++ iHog/Views/Settings/Device/ProgrammerSettings.swift
@@ -0,0 +1,20 @@
+//
+//  ProgrammerSettings.swift
+//  iHog
+//
+//  Created by Maegan Wilson on 3/24/21.
+//
+
+import SwiftUI
+
+struct ProgrammerSettings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProgrammerSettings_Previews: PreviewProvider {
+    static var previews: some View {
+        ProgrammerSettings()
+    }
+}
