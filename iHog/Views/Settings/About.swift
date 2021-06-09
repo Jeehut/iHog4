9f696da83ae36953c7056548dbef22888dafb400
Maegan <maegan@maeganwilson.com>
2021-06-09 10:38:54 -0500

    Broke out About section to it's own file

--- iHog/Views/Settings/About.swift
+++ iHog/Views/Settings/About.swift
@@ -8,13 +8,31 @@
 import SwiftUI
 
 struct About: View {
+    @Binding var selectedSetting: SettingsNav?
+    var totalTipped: Double
+    
+    let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+    let appBuild = Bundle.main.infoDictionary?["CFBundleVersion"] as? String
+    
     var body: some View {
-        Text("Hello, World!")
+        Section(header: Text("About"),
+                footer: Text("App Version: \(appVersion ?? "N/A") (\(appBuild ?? "N/A"))")){
+            Link("\(Image(systemName: "info.circle")) About [iHog Website]", destination: URL(string: "https://ihogapp.com/about")!)
+            Link("\(Image(systemName: "ant")) Report a bug [GitHub Account Required]", destination: URL(string: "https://github.com/maeganwilson/iHog4/issues/new?assignees=maeganwilson&labels=question&template=bug_report.md&title=%5BBUG%5D")!)
+            Link("\(Image(systemName: "lightbulb")) Request a feature [GitHub Account Required]", destination: URL(string: "https://github.com/maeganwilson/iHog4/issues/new?assignees=maeganwilson&labels=question&template=feature_request.md&title=%5BREQUEST%5D")!)
+            NavigationLink(
+                "\(Image(systemName: "dollarsign.circle")) Tip Jar (\(SettingsView.priceFormatter.string(from: NSNumber(value: totalTipped)) ?? "NONE"))",
+                destination: TipJarView(),
+                tag: SettingsNav.tipJar,
+                selection: $selectedSetting)
+//                        Link("📘 Guide [iHog Website]", destination: URL(string: "https://ihogapp.com/guide")!)
+            Link("\(Image(systemName: "bubble.left")) Chat about iHog [Dev's discord link]", destination: URL(string: "https://discord.gg/HmGYbNHmun")!)
+        }
     }
 }
 
-struct About_Previews: PreviewProvider {
-    static var previews: some View {
-        About()
-    }
-}
+//struct About_Previews: PreviewProvider {
+//    static var previews: some View {
+//        About()
+//    }
+//}
--- iHog/Views/Settings/SettingsView.swift
+++ iHog/Views/Settings/SettingsView.swift
@@ -32,9 +32,6 @@ struct SettingsView: View {
         return formatter
     }()
     
-    let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
-    let appBuild = Bundle.main.infoDictionary?["CFBundleVersion"] as? String
-    
     
     var body: some View {
         NavigationView{
@@ -71,19 +68,7 @@ struct SettingsView: View {
                         NavigationLink("OSC Log", destination: OSCLogView(), tag: SettingsNav.oscLogView, selection: $selectedSetting)
                     }
                     // MARK: ABOUT
-                    Section(header: Text("About"),
-                            footer: Text("App Version: \(appVersion ?? "N/A") (\(appBuild ?? "N/A"))")){
-                        Link("\(Image(systemName: "info.circle")) About [iHog Website]", destination: URL(string: "https://ihogapp.com/about")!)
-                        Link("\(Image(systemName: "ant")) Report a bug [GitHub Account Required]", destination: URL(string: "https://github.com/maeganwilson/iHog4/issues/new?assignees=maeganwilson&labels=question&template=bug_report.md&title=%5BBUG%5D")!)
-                        Link("\(Image(systemName: "lightbulb")) Request a feature [GitHub Account Required]", destination: URL(string: "https://github.com/maeganwilson/iHog4/issues/new?assignees=maeganwilson&labels=question&template=feature_request.md&title=%5BREQUEST%5D")!)
-                        NavigationLink(
-                            "\(Image(systemName: "dollarsign.circle")) Tip Jar (\(SettingsView.priceFormatter.string(from: NSNumber(value: totalTipped)) ?? "NONE"))",
-                            destination: TipJarView(),
-                            tag: SettingsNav.tipJar,
-                            selection: $selectedSetting)
-//                        Link("📘 Guide [iHog Website]", destination: URL(string: "https://ihogapp.com/guide")!)
-                        Link("\(Image(systemName: "bubble.left")) Chat about iHog [Dev's discord link]", destination: URL(string: "https://discord.gg/HmGYbNHmun")!)
-                    }
+                    About(selectedSetting: $selectedSetting, totalTipped: totalTipped)
                 }
                 .listStyle( SidebarListStyle())
                 .blur(radius: isAddingShow ? 2.5 : 0.0)
