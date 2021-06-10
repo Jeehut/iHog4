10c893a7cc979690c1e30825be461f2d6c1a9bac
maegan <maegan@maeganwilson.com>
2021-06-09 23:07:02 -0500

    Added tests for updating everything

--- iHogTests/ChosenShowTests.swift
+++ iHogTests/ChosenShowTests.swift
@@ -42,7 +42,7 @@ class ChosenShowTests: XCTestCase {
         XCTAssertEqual(sut.scenes, [testScene])
     }
     
-    func test_ChosenShowAddScenes_ShouldHave10Scenes() {
+    func test_ChosenShowAddScenes_ShouldHave500Scenes() {
         
         for n in 1...500 {
             let tempScene = ShowObject(objType: .scene,
@@ -52,6 +52,12 @@ class ChosenShowTests: XCTestCase {
         }
         
         XCTAssertEqual(sut.scenes.count, 500)
+    }
+    
+    func test_ChosenShowAddScenes_ShouldNotHaveScene(){
+        let testObj = ShowObject(objType: .list, number: 1, objColor: "green")
+        sut.addScene(testObj)
+        XCTAssertEqual(sut.scenes.count, 0)
     }
     
     func test_ChosenShowUpdateSceneName_ShouldHaveOut(){
@@ -82,6 +88,20 @@ class ChosenShowTests: XCTestCase {
         XCTAssertEqual(sut.scenes[10].getName(), "Out")
     }
     
+    func test_ChosenShowUpdateScene_ShouldNotChange(){
+        let SAMPLE_SCENE = ShowObject(objType: .scene,
+                                      number: 1,
+                                      objColor: "red")
+        sut.addScene(SAMPLE_SCENE)
+        
+        var updatedScene = SAMPLE_SCENE
+        updatedScene.objType = .list
+        updatedScene.setName("Testing")
+        
+        sut.updateScene(updatedScene)
+        XCTAssertEqual(sut.scenes[0].getName(), "Scene 1")
+    }
+    
     func test_RemoveScene_ShouldHave0Scenes(){
         let SAMPLE_SCENE = ShowObject(objType: .scene,
                                       number: 1,
@@ -103,6 +123,22 @@ class ChosenShowTests: XCTestCase {
         
         sut.removeScene(removedScene)
         XCTAssertFalse(sut.scenes[10] == removedScene)
+    }
+    
+    func test_RemoveScene_ShouldNotChange(){
+        let SAMPLE_SCENE = ShowObject(objType: .scene,
+                                      number: 1,
+                                      objColor: "red")
+        
+        let SAMPLE_LIST = ShowObject(objType: .list,
+                                          number: 1,
+                                          objColor: "red")
+        sut.addScene(SAMPLE_LIST)
+        sut.addScene(SAMPLE_SCENE)
+        
+        sut.removeScene(SAMPLE_LIST)
+        XCTAssertEqual(sut.scenes[0].getName(), "Scene 1")
+        
     }
     
     // MARK: Testing Lists
@@ -176,5 +212,93 @@ class ChosenShowTests: XCTestCase {
         
         sut.removeList(removedList)
         XCTAssertFalse(sut.lists[10] == removedList)
+    }
+    
+    // MARK: Testing Groups
+    func test_ChosenShowAddGroups_ShouldHave1Group() {
+        let SAMPLE_GROUP = ShowObject(objType: .group, number: 1, objColor: "red")
+        sut.addGroup(SAMPLE_GROUP)
+        XCTAssertEqual(sut.groups, [SAMPLE_GROUP])
+    }
+    
+    func test_ChosenShowAddGroups_ShouldHave500Groups() {
+        for n in 1...500 {
+            let tempGroup = ShowObject(objType: .group, number: Double(n), objColor: "red")
+            
+            sut.addGroup(tempGroup)
+        }
+        
+        XCTAssertEqual(sut.groups.count, 500)
+    }
+    
+    func test_ChosenShowAddGroup_ShouldNotHaveGroup(){
+        let testObj = ShowObject(objType: .list, number: 1, objColor: "green")
+        sut.addGroup(testObj)
+        XCTAssertEqual(sut.groups.count, 0)
+    }
+    
+    func test_ChosenShowUpdateGroupName_ShouldHaveOut(){
+        let SAMPLE_GROUP = ShowObject(objType: .group, number: 1, objColor: "red")
+        sut.addGroup(SAMPLE_GROUP)
+        
+        var updatedGroup = SAMPLE_GROUP
+        updatedGroup.setName("OUT")
+        
+        sut.updateGroup(updatedGroup)
+        
+        XCTAssertEqual(sut.groups[0].getName(), "OUT")
+    }
+    
+    func test_RemoveGroup_ShouldHave0Groups() {
+        let SAMPLE_GROUP = ShowObject(objType: .group, number: 1, objColor: "red")
+        sut.addGroup(SAMPLE_GROUP)
+        
+        sut.removeGroup(SAMPLE_GROUP)
+        
+        XCTAssertEqual(sut.groups.count, 0)
+    }
+    
+    // MARK: Testing Palettes
+    func test_ChosenShowAddPalettes_ShouldHave1Palette() {
+        let SAMPLE_PALETTE = ShowObject(objType: .position, number: 1, objColor: "red")
+        sut.addPalette(SAMPLE_PALETTE)
+        XCTAssertEqual(sut.palettes, [SAMPLE_PALETTE])
+    }
+    
+    func test_ChosenShowAddPalettes_ShouldHave500Palettes() {
+        for n in 1...500 {
+            let tempPalette = ShowObject(objType: .position, number: Double(n), objColor: "red")
+            
+            sut.addPalette(tempPalette)
+        }
+        
+        XCTAssertEqual(sut.palettes.count, 500)
+    }
+    
+    func test_ChosenShowAddPalette_ShouldNotHavePalette(){
+        let testObj = ShowObject(objType: .group, number: 1, objColor: "green")
+        sut.addPalette(testObj)
+        XCTAssertEqual(sut.palettes.count, 0)
+    }
+    
+    func test_ChosenShowUpdatePaletteName_ShouldHaveOut(){
+        let SAMPLE_PALETTE = ShowObject(objType: .position, number: 1, objColor: "red")
+        sut.addPalette(SAMPLE_PALETTE)
+        
+        var updatedPalette = SAMPLE_PALETTE
+        updatedPalette.setName("OUT")
+        
+        sut.updatePalette(updatedPalette)
+        
+        XCTAssertEqual(sut.palettes[0].getName(), "OUT")
+    }
+    
+    func test_RemovePalette_ShouldHave0Palettes() {
+        let SAMPLE_PALETTE = ShowObject(objType: .position, number: 1, objColor: "red")
+        sut.addPalette(SAMPLE_PALETTE)
+        
+        sut.removePalette(SAMPLE_PALETTE)
+        
+        XCTAssertEqual(sut.palettes.count, 0)
     }
 }
