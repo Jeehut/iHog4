d9af77784e03533206c54a57907ffd7c42effdc7
maegan <maegan@maeganwilson.com>
2021-06-09 20:53:04 -0500

    Added guards to only add if proper type

--- iHog/Model/ChosenShow.swift
+++ iHog/Model/ChosenShow.swift
@@ -25,80 +25,113 @@ class ChosenShow: ObservableObject {
     
     // MARK: Scenes
     func addScene(_ obj: ShowObject) {
-        scenes.append(obj)
+        if obj.objType == .scene {
+            scenes.append(obj)
+        }
     }
     
     func updateScene(_ obj: ShowObject) {
-        let index = scenes.firstIndex{ $0.id == obj.id }
-        if index != nil {
-            scenes[index!].setName(obj.getName())
-            scenes[index!].setNumber(obj.number)
-            scenes[index!].setColor(obj.getColorString())
-        } else {
-            print("UPDATE SHOULD NOT HAVE BEEN CALLED")
+        if obj.objType == .scene {
+            let index = scenes.firstIndex{ $0.id == obj.id }
+            if index != nil {
+                scenes[index!].setName(obj.getName())
+                scenes[index!].setNumber(obj.number)
+                scenes[index!].setColor(obj.getColorString())
+            } else {
+                print("UPDATE SHOULD NOT HAVE BEEN CALLED")
+            }
         }
     }
     
     func removeScene(_ obj: ShowObject) {
-        scenes.removeAll{ $0.id == obj.id }
+        if obj.objType == .scene {
+            scenes.removeAll{ $0.id == obj.id }
+        }
     }
     // MARK: Lists
     func addList(_ obj: ShowObject) {
-        lists.append(obj)
+        if obj.objType == .list {
+            lists.append(obj)
+        }
     }
     
     func updateList(_ obj: ShowObject) {
-        let index = lists.firstIndex{ $0.id == obj.id }
-        if index != nil {
-            lists[index!].setName(obj.getName())
-            lists[index!].setNumber(obj.number)
-            lists[index!].setColor(obj.getColorString())
-        } else {
-            print("UPDATE SHOULD NOT HAVE BEEN CALLED")
+        if obj.objType == .list {
+            let index = lists.firstIndex{ $0.id == obj.id }
+            if index != nil {
+                lists[index!].setName(obj.getName())
+                lists[index!].setNumber(obj.number)
+                lists[index!].setColor(obj.getColorString())
+            } else {
+                print("UPDATE SHOULD NOT HAVE BEEN CALLED")
+            }
         }
     }
     
     func removeList(_ obj: ShowObject) {
-        lists.removeAll{ $0.id == obj.id }
+        if obj.objType == .list {
+            lists.removeAll{ $0.id == obj.id }
+        }
     }
     
     // MARK: Groups
     func addGroup(_ obj: ShowObject) {
-        groups.append(obj)
+        if obj.objType == .group {
+            groups.append(obj)
+        }
     }
     
     func updateGroup(_ obj: ShowObject) {
-        let index = groups.firstIndex{ $0.id == obj.id }
-        if index != nil {
-            groups[index!].setName(obj.getName())
-            groups[index!].setNumber(obj.number)
-            groups[index!].setColor(obj.getColorString())
-        } else {
-            print("UPDATE SHOULD NOT HAVE BEEN CALLED")
+        if obj.objType == .group {
+            let index = groups.firstIndex{ $0.id == obj.id }
+            if index != nil {
+                groups[index!].setName(obj.getName())
+                groups[index!].setNumber(obj.number)
+                groups[index!].setColor(obj.getColorString())
+            } else {
+                print("UPDATE SHOULD NOT HAVE BEEN CALLED")
+            }
         }
     }
     
     func removeGroup(_ obj: ShowObject) {
-        groups.removeAll{ $0.id == obj.id }
+        if obj.objType == .group {
+            groups.removeAll{ $0.id == obj.id }
+        }
     }
     
     // MARK: Palettes
     func addPalette(_ obj: ShowObject) {
-        palettes.append(obj)
+        switch obj.objType {
+        case .intensity, .color, .position, .effect, .beam:
+            palettes.append(obj)
+        default:
+            break
+        }
     }
     
     func updatePalette(_ obj: ShowObject) {
-        let index = palettes.firstIndex{ $0.id == obj.id }
-        if index != nil {
-            palettes[index!].setName(obj.getName())
-            palettes[index!].setNumber(obj.number)
-            palettes[index!].setColor(obj.getColorString())
-        } else {
-            print("UPDATE SHOULD NOT HAVE BEEN CALLED")
+        switch obj.objType {
+        case .intensity, .color, .position, .effect, .beam:
+            let index = palettes.firstIndex{ $0.id == obj.id }
+            if index != nil {
+                palettes[index!].setName(obj.getName())
+                palettes[index!].setNumber(obj.number)
+                palettes[index!].setColor(obj.getColorString())
+            } else {
+                print("UPDATE SHOULD NOT HAVE BEEN CALLED")
+            }
+        default:
+            break
         }
     }
     
     func removePalette(_ obj: ShowObject) {
-        palettes.removeAll{ $0.id == obj.id }
+        switch obj.objType {
+        case .intensity, .color, .position, .effect, .beam:
+            palettes.removeAll{ $0.id == obj.id }
+        default:
+            break
+        }
     }
 }
