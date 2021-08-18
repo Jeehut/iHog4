48a3ddace69c63daf3d8abcf7601d91b65a41322
Maegan <maegan@maeganwilson.com>
2021-08-17 20:59:29 -0500

    #49 Fixes layout issue

--- iHog/Views/Hardware/Programming/FPParts/EncoderWheelsView.swift
+++ iHog/Views/Hardware/Programming/FPParts/EncoderWheelsView.swift
@@ -10,37 +10,71 @@ import SwiftUI
 struct EncoderWheelsView: View {
     @EnvironmentObject var osc: OSCHelper
     @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @Environment(\.verticalSizeClass) var verticalSizeClass
     @AppStorage(Settings.isNanoModeOn.rawValue) var isNanoModeOn: Bool = false
     @AppStorage(Settings.encoderWheelPrecision.rawValue) var encoderWheelPrecision: Double = 2.0
     @AppStorage(Settings.isEncoderFine.rawValue) var isEncoderFine: Bool = false
     
     var body: some View {
-        if horizontalSizeClass == .regular {
+        switch horizontalSizeClass {
+        case .regular:
             // ipad or room for 5 encoders
-            VStack{
-                HStack{
-                    EncoderWheel(encoderWheelNum: 1, paramName: osc.encoderWheelLabels[0], paramValue: osc.encoderWheelValues[0])
-                    EncoderWheel(encoderWheelNum: 2, paramName: osc.encoderWheelLabels[1], paramValue: osc.encoderWheelValues[1])
-                    EncoderWheel(encoderWheelNum: 3, paramName: osc.encoderWheelLabels[2], paramValue: osc.encoderWheelValues[2])
-                    EncoderWheel(encoderWheelNum: 4, paramName: osc.encoderWheelLabels[3], paramValue: osc.encoderWheelValues[3])
-                    if isNanoModeOn == false {
-                        EncoderWheel(encoderWheelNum: 5, paramName: osc.encoderWheelLabels[4], paramValue: osc.encoderWheelValues[4])
-                    }
-                }
-                HStack{
-                    Button(action: {
-                        isEncoderFine.toggle()
-                        if isEncoderFine {
-                            encoderWheelPrecision = encoderWheelPrecision / 2.0
-                        } else {
-                            encoderWheelPrecision = encoderWheelPrecision * 2.0
+            switch verticalSizeClass {
+            case .regular:
+                VStack{
+                    Spacer()
+                    HStack{
+                        EncoderWheel(encoderWheelNum: 1, paramName: osc.encoderWheelLabels[0], paramValue: osc.encoderWheelValues[0])
+                        EncoderWheel(encoderWheelNum: 2, paramName: osc.encoderWheelLabels[1], paramValue: osc.encoderWheelValues[1])
+                        EncoderWheel(encoderWheelNum: 3, paramName: osc.encoderWheelLabels[2], paramValue: osc.encoderWheelValues[2])
+                        EncoderWheel(encoderWheelNum: 4, paramName: osc.encoderWheelLabels[3], paramValue: osc.encoderWheelValues[3])
+                        if isNanoModeOn == false {
+                            EncoderWheel(encoderWheelNum: 5, paramName: osc.encoderWheelLabels[4], paramValue: osc.encoderWheelValues[4])
                         }
-                    }) {
-                        Text("Fine Control \(isEncoderFine ? "ON" : "OFF")")
                     }
+                    HStack{
+                        Button(action: {
+                            isEncoderFine.toggle()
+                            if isEncoderFine {
+                                encoderWheelPrecision = encoderWheelPrecision / 2.0
+                            } else {
+                                encoderWheelPrecision = encoderWheelPrecision * 2.0
+                            }
+                        }) {
+                            Text("Fine Control \(isEncoderFine ? "ON" : "OFF")")
+                        }
+                    }
+                    Spacer()
+                }
+            default:
+                VStack(alignment: .leading){
+                    HStack{
+                        EncoderWheel(encoderWheelNum: 1, paramName: osc.encoderWheelLabels[0], paramValue: osc.encoderWheelValues[0])
+                        EncoderWheel(encoderWheelNum: 2, paramName: osc.encoderWheelLabels[1], paramValue: osc.encoderWheelValues[1])
+                        EncoderWheel(encoderWheelNum: 3, paramName: osc.encoderWheelLabels[2], paramValue: osc.encoderWheelValues[2])
+                    }
+                    HStack{
+                        EncoderWheel(encoderWheelNum: 4, paramName: osc.encoderWheelLabels[3], paramValue: osc.encoderWheelValues[3])
+                        if isNanoModeOn == false {
+                            EncoderWheel(encoderWheelNum: 5, paramName: osc.encoderWheelLabels[4], paramValue: osc.encoderWheelValues[4])
+                        }
+                    }
+                    HStack{
+                        Button(action: {
+                            isEncoderFine.toggle()
+                            if isEncoderFine {
+                                encoderWheelPrecision = encoderWheelPrecision / 2.0
+                            } else {
+                                encoderWheelPrecision = encoderWheelPrecision / 2.0
+                            }
+                        }) {
+                            Text("Fine Control \(isEncoderFine ? "ON" : "OFF")")
+                        }
+                        .padding(.vertical)
+                    }.padding(.horizontal)
                 }
             }
-        } else {
+        default:
             VStack(alignment: .leading){
                 HStack{
                     EncoderWheel(encoderWheelNum: 1, paramName: osc.encoderWheelLabels[0], paramValue: osc.encoderWheelValues[0])
