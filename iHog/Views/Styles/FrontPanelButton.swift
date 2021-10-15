fcdcdc380e651ef82f62920a1c415d945a756d5e
Maegan <maegan@maeganwilson.com>
2021-10-15 06:57:17 -0500

    Cleaned up unused code

--- iHog/Views/Styles/FrontPanelButton.swift
+++ iHog/Views/Styles/FrontPanelButton.swift
@@ -24,15 +24,3 @@ struct FrontPanelButton: ButtonStyle {
             .scaleEffect(configuration.isPressed ? 0.9 : 1.0)
     }
 }
-
-//struct GrowingButton: ButtonStyle {
-//    func makeBody(configuration: Configuration) -> some View {
-//        configuration.label
-//            .padding()
-//            .background(Color.blue)
-//            .foregroundColor(.white)
-//            .clipShape(Capsule())
-//            .scaleEffect(configuration.isPressed ? 1.2 : 1)
-//            .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
-//    }
-//}
