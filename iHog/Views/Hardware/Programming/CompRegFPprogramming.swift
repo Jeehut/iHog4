d75dd9fde2d8d83e2b85d5eed336c2fe07ca1d64
Maegan <maegan@maeganwilson.com>
2021-10-19 06:47:12 -0500

    Adjusting VStack to get rid of that invisible hstack

--- iHog/Views/Hardware/Programming/CompRegFPprogramming.swift
+++ iHog/Views/Hardware/Programming/CompRegFPprogramming.swift
@@ -18,44 +18,45 @@ struct CompRegFPprogramming: View {
     var body: some View {
         VStack{
             ScrollView{
-                HStack(){Spacer()}
-                HBCButtonView()
-                // Used to open windows with different sets of buttons in them.
-                HStack{
-                    // Encoders
-                    Button {
-                        isEncoderShown.toggle()
-                    } label: {
-                        Image(systemName: "dial.max")
-                    }.buttonStyle(FrontPanelButton(width: 65, backgroundColor: .systemGray3))
-                        .sheet(isPresented: $isEncoderShown, content: {
-                            EncodersKindsSheet()
-                                .environmentObject(osc)
-                        })
-                    // Object Keys, Action Keys, & utility keys
-                    Button {
-                        isActionShown.toggle()
-                    } label: {
-                        Image(systemName: "rectangle.3.offgrid")
-                    }.buttonStyle(FrontPanelButton(width: 65, backgroundColor: .systemGray3))
-                        .sheet(isPresented: $isActionShown) {
-                            isActionShown = false
-                        } content: {
-                            ObjActUtilKeys().environmentObject(osc)
-                        }
-                    // Function keys
-                    Button {
-                        isFunctionShown.toggle()
-                    } label: {
-                        Image(systemName: "square.grid.3x3")
-                    }.buttonStyle(FrontPanelButton(width: 65, backgroundColor: .systemGray3))
-                        .sheet(isPresented: $isFunctionShown) {
-                            isFunctionShown = false
-                        } content: {
-                            FunctionKeySheet().environmentObject(osc)
-                        }
+                LazyVStack{
+                    HBCButtonView()
+                    // Used to open windows with different sets of buttons in them.
+                    HStack{
+                        // Encoders
+                        Button {
+                            isEncoderShown.toggle()
+                        } label: {
+                            Image(systemName: "dial.max")
+                        }.buttonStyle(FrontPanelButton(width: 65, backgroundColor: .systemGray3))
+                            .sheet(isPresented: $isEncoderShown, content: {
+                                EncodersKindsSheet()
+                                    .environmentObject(osc)
+                            })
+                        // Object Keys, Action Keys, & utility keys
+                        Button {
+                            isActionShown.toggle()
+                        } label: {
+                            Image(systemName: "rectangle.3.offgrid")
+                        }.buttonStyle(FrontPanelButton(width: 65, backgroundColor: .systemGray3))
+                            .sheet(isPresented: $isActionShown) {
+                                isActionShown = false
+                            } content: {
+                                ObjActUtilKeys().environmentObject(osc)
+                            }
+                        // Function keys
+                        Button {
+                            isFunctionShown.toggle()
+                        } label: {
+                            Image(systemName: "square.grid.3x3")
+                        }.buttonStyle(FrontPanelButton(width: 65, backgroundColor: .systemGray3))
+                            .sheet(isPresented: $isFunctionShown) {
+                                isFunctionShown = false
+                            } content: {
+                                FunctionKeySheet().environmentObject(osc)
+                            }
+                    }
+                    SelectButtonView()
                 }
-                SelectButtonView()
             }.padding(.bottom, BASE_PADDING)
             Spacer()
             NumericKeypadView()
