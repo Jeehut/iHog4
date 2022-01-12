87c68354708c22df314a6185bf4ed557f4468c5f
Maegan Wilson <maegan@maeganwilson.com>
2022-01-12 08:00:48 -0600

    Adding delegates for tcp also

--- iHog/Helpers/OSC/OSCHelper+Server.swift
+++ iHog/Helpers/OSC/OSCHelper+Server.swift
@@ -31,3 +31,46 @@ extension OSCHelper: OSCUdpServerDelegate {
         print("Server did read data with error: \(error.localizedDescription)")
     }
 }
+
+extension OSCHelper: OSCTcpServerDelegate {
+    func server(_ server: OSCTcpServer,
+                didConnectToClientWithHost host: String,
+                port: UInt16) {
+        print("Server did connect to client \(host):\(port)")
+    }
+
+    func server(_ server: OSCTcpServer,
+                didDisconnectFromClientWithHost host: String,
+                port: UInt16) {
+        print("Server did disconnect from client \(host):\(port)")
+    }
+
+    func server(_ server: OSCTcpServer,
+                didReceivePacket packet: OSCPacket,
+                fromHost host: String,
+                port: UInt16) {
+        print("Server did receive packet")
+    }
+
+    func server(_ server: OSCTcpServer,
+                didSendPacket packet: OSCPacket,
+                toClientWithHost host: String,
+                port: UInt16) {
+        print("Server did send packet to \(host):\(port)")
+    }
+
+    func server(_ server: OSCTcpServer,
+                socketDidCloseWithError error: Error?) {
+        if let error = error {
+           print("server did stop listening with error: \(error.localizedDescription)")
+        } else {
+           print("server did stop listening")
+        }
+    }
+
+    func server(_ server: OSCTcpServer,
+                didReadData data: Data,
+                with error: Error) {
+        print("Server did read data with error: \(error.localizedDescription)"
+    }
+}
--- iHog/Helpers/OSC/OSCHelper.swift
+++ iHog/Helpers/OSC/OSCHelper.swift
@@ -10,5 +10,20 @@ import Combine
 import OSCKit
 
 class OSCHelper {
-    
+    private var consoleIP: String
+    private var consoleInputPort: Int
+    private var consoleOutputPort: Int
+    private var useTCP: Bool
+
+    private var udpClient: OSCUdpClient?
+    private var updServer: OSCUdpServer?
+    private var tcpClient: OSCTcpClient?
+    private var tcpServer: OSCTcpServer?
+
+    init(ip: String, inputPort: Int, outputPort: Int) {
+        consoleIP = ip
+        consoleInputPort = inputPort
+        consoleOutputPort = outputPort
+        useTCP = false
+    }
 }
