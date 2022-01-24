1de4aeb83a4df7b0be27041e0b50f568fbc2d09f
Maegan Wilson <maegan@maeganwilson.com>
2022-01-24 07:21:44 -0600

    Adjusting view

--- iHog/Views/Settings/NewVersionView.swift
+++ iHog/Views/Settings/NewVersionView.swift
@@ -17,38 +17,34 @@ struct NewVersionView: View {
     let appBuild = Bundle.main.infoDictionary?["CFBundleVersion"] as! String
 
     var body: some View {
-        VStack {
-            CloseButton()
-            Text("v\(appVersion) (\(appBuild))")
-                .font(.largeTitle)
-            List {
-                Section {
-                    Text("Focus View")
-                    Text("OSC improvements")
-                } header: {
-                    Text("✅ Enhancements")
-                }
-                Section {
-                    Text("Crash when connect to other device on the network")
-                    Text("Group selection was not working")
-                    Text("Function keys were not receiving labels")
-                } header: {
-                    Text("❌ Fixes")
-                }
-                Section {
-                    Text("Thanks for supporting iHog and continuing to use it!").lineLimit(nil)
-                    Button("⭐ Leave a review ⭐") {
-                        let url = URL(string: "itms-apps://itunes.apple.com/app/1487580623")
-                        UIApplication.shared.open(url!)
-                    }.foregroundColor(.primary)
-                    Button("☕ Buy Maegan a Coffee ☕") {
-                        let url = URL(string: "itms-apps://itunes.apple.com/app/1487580623")
-                        UIApplication.shared.open(url!)
-                    }.foregroundColor(.primary)
-                } header: {
-                    Text("Support iHog's Development")
-                }
+        List {
+            Section {
+                Text("Focus View")
+                Text("OSC improvements")
+            } header: {
+                Text("✅ Enhancements")
             }
-        }
+            Section {
+                Text("Crash when connect to other device on the network")
+                Text("Group selection was not working")
+                Text("Function keys were not receiving labels")
+            } header: {
+                Text("❌ Fixes")
+            }
+            Section {
+                Text("Thanks for supporting iHog and continuing to use it!").lineLimit(nil)
+                Button("⭐ Leave a review ⭐") {
+                    let url = URL(string: "itms-apps://itunes.apple.com/app/1487580623")
+                    UIApplication.shared.open(url!)
+                }.foregroundColor(.primary)
+                Button("\(Image(systemName: "cup.and.saucer")) Buy Maegan a Coffee") {
+                    let url = URL(string: "itms-apps://itunes.apple.com/app/1487580623")
+                    UIApplication.shared.open(url!)
+                }.foregroundColor(.primary)
+            } header: {
+                Text("Support iHog's Development")
+            }
+        }.navigationTitle(Text("v\(appVersion) (\(appBuild))"))
+            .navigationBarTitleDisplayMode(.large)
     }
 }
