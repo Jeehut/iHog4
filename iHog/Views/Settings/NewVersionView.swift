0352e8e9deff050460ea4e0c9294578bcf49e079
Maegan Wilson <maegan@maeganwilson.com>
2022-01-24 07:35:11 -0600

    Working on buttons

--- iHog/Views/Settings/NewVersionView.swift
+++ iHog/Views/Settings/NewVersionView.swift
@@ -29,6 +29,7 @@ struct NewVersionView: View {
                     Text("Crash when connect to other device on the network")
                     Text("Group selection was not working")
                     Text("Function keys were not receiving labels")
+                    Text("Release notes in app")
                 } header: {
                     Text("❌ Fixes")
                 }
@@ -45,6 +46,36 @@ struct NewVersionView: View {
                 } header: {
                     Text("Support iHog's Development")
                 }
+            }
+            HStack(spacing: 20)  {
+                Button {
+                    let url = URL(string: "itms-apps://itunes.apple.com/app/1487580623")
+                    UIApplication.shared.open(url!)
+                } label: {
+                    VStack(spacing: 20) {
+                        HStack {
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.fill")
+                        }.foregroundColor(.orange)
+                        Text("Leave a Review")
+                            .font(.headline)
+                            .lineLimit(nil)
+                        Text("A review helps other people find the app and let's Maegan know if they are working in the right direction.")
+                            .lineLimit(nil)
+                    }
+                    VStack(spacing: 20)  {
+                        Image(systemName: "cup.and.saucer")
+                        Text("Buy Maegan a Coffee")
+                            .font(.headline)
+                            .lineLimit(nil)
+                        Text("Support an solo developer")
+                            .lineLimit(nil)
+                    }
+                }
+
             }
         }.navigationTitle(Text("v\(appVersion) (\(appBuild))"))
             .navigationBarTitleDisplayMode(.large)
